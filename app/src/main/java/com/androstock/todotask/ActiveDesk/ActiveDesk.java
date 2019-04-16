d9f5858d789f9f4a406baf38e470a5502af3adea
Dmitry-Karchevsky <dima.game2010@yandex.ru>
2019-04-16 21:22:43 +0300

    перезалив, который вроде как работает

--- app/src/main/java/com/androstock/todotask/ActiveDesk/ActiveDesk.java
+++ app/src/main/java/com/androstock/todotask/ActiveDesk/ActiveDesk.java
@@ -1,11 +1,17 @@
 package com.androstock.todotask.ActiveDesk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.androstock.todotask.Home.HomeActivity;
+import com.androstock.todotask.R;
+import com.androstock.todotask.Task.TaskHome;
+import com.androstock.todotask.chat.Chat_test;
 
 public class ActiveDesk extends AppCompatActivity {
 
@@ -17,14 +23,20 @@ public class ActiveDesk extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_main:
+
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_daily_log:
+                    //открывает ежедневник
+                    Intent intent = new Intent(ActiveDesk.this, TaskHome.class);
+                    startActivity(intent);
+                    break;
+                case R.id.navigation_chat:
+                    Intent intent1 = new Intent(ActiveDesk.this, Chat_test.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.navigation_task_board:
+                    //открывает доску задач
                     return true;
             }
             return false;
