dc171ac5d6cc3d168f9a2d50123262e84b385db7
chekanovkin <ymnyaga@yandex.ru>
2019-05-06 12:31:18 +0300

    Криво запушил, теперь должно быть без ошибок

---
+++ app/src/main/java/com/androstock/todotask/Calendar/DrawableUtils.java
@@ -0,0 +1,27 @@
+package com.androstock.todotask.Calendar;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.InsetDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.support.v4.content.ContextCompat;
+
+import com.androstock.todotask.R;
+
+/**
+ * Created by Mateusz Kornakiewicz on 02.08.2018.
+ */
+
+public final class DrawableUtils {
+
+
+    public static Drawable getThreeDots(Context context){
+        Drawable drawable = ContextCompat.getDrawable(context, R.drawable.sample_three_icons);
+
+        //Add padding to too large icon
+        return new InsetDrawable(drawable, 100, 0, 100, 0);
+    }
+
+    private DrawableUtils() {
+    }
+}
