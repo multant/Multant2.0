f8db390b1336c5312884c8b69c383a6514464d9e
chekanovkin <ymnyaga@yandex.ru>
2019-05-22 12:06:45 +0300

    откатываюсь нахуй назад

--- app/src/main/java/com/androstock/multant/Diary/Entries.java
+++ app/src/main/java/com/androstock/multant/Diary/Entries.java
@@ -2,8 +2,6 @@ package com.androstock.multant.Diary;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -11,20 +9,15 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.support.*;
 
 import com.androstock.multant.ActiveDesk.ActiveDesk;
 import com.androstock.multant.DB.Function;
@@ -33,11 +26,11 @@ import com.androstock.multant.Home.HomeActivity;
 import com.androstock.multant.R;
 import com.androstock.multant.Task.TaskHome;
 import com.androstock.multant.chat.Chat_test;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
@@ -182,52 +175,9 @@ public class Entries extends AppCompatActivity {
     }
 
     public void showStartDatePicker(View v) {
-        DialogFragment dialogFragment = new DatePickerDialogTheme3();
-        Bundle bundle = new Bundle();
-        bundle.putInt("whichDate", 1);
-        dialogFragment.setArguments(bundle);
-        dialogFragment.show(getSupportFragmentManager(), "Theme 3");
-    }
-
-    public void showFinishDatePicker(View v) {
-        DialogFragment dialogFragment = new DatePickerDialogTheme3();
-        Bundle bundle = new Bundle();
-        bundle.putInt("whichDate", 2);
-        dialogFragment.setArguments(bundle);
-        dialogFragment.show(getSupportFragmentManager(), "Theme 3");
-    }
-
-    public static class DatePickerDialogTheme3 extends DialogFragment implements DatePickerDialog.OnDateSetListener{
-
-
-        int whichDate;
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState){
-            final Calendar calendar = Calendar.getInstance();
-            int year = calendar.get(Calendar.YEAR);
-            int month = calendar.get(Calendar.MONTH);
-            int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-            DatePickerDialog datepickerdialog = new DatePickerDialog(getActivity(),
-                    AlertDialog.THEME_HOLO_DARK, this, year, month, day);
-
-            Bundle args = getArguments();
-            whichDate = args.getInt("whichDate");
-            return datepickerdialog;
-        }
-
-        public void onDateSet(DatePicker view, int year, int month, int day){
-
-            if(whichDate == 1){
-                EditText editText = (EditText) getActivity().findViewById(R.id.start_date_text);
-                editText.setText(day + "/" + (month+1) + "/" + year);
-            } else if(whichDate == 2){
-                EditText editText = (EditText) getActivity().findViewById(R.id.end_date_text);
-                editText.setText(day + "/" + (month+1) + "/" + year);
-            }
-
-        }
+        //
+        Toast.makeText(getApplicationContext(),"Вызов datepicker",Toast.LENGTH_SHORT).show();
+        //
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
