ddb7ced7aa3754b3b801afa293b3c35d8393094f
n3legal <nikita.shachnov@yandex.ru>
2019-05-31 23:49:36 +0300

    всплывающее оено и колонны

--- app/src/main/java/com/androstock/multant/ActiveDesk/ActiveDeskPage.java
+++ app/src/main/java/com/androstock/multant/ActiveDesk/ActiveDeskPage.java
@@ -1,12 +1,20 @@
 package com.androstock.multant.ActiveDesk;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -20,49 +28,81 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ActiveDeskPage extends FragmentActivity {
 
     private FirebaseAuth mAuth;
     private DatabaseReference myRef;
 
-
-    Desk desk = new Desk();
-
-
-
-    //private Desk desk = new Desk("", "", "", 0);
-
-    private String name_desk = "";
     private String id_desk = "";
+
+    ViewPager pager;
+    static final int PAGE_COUNT = 1;
+
+    final List<Column> columns = new ArrayList<Column>();
+
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.active_desk_page);
         this.id_desk = getIntent().getExtras().getString("id_desk");
-        desk.setId(id_desk);
         FirebaseUser user = mAuth.getInstance().getCurrentUser();
 
         myRef = FirebaseDatabase.getInstance().getReference();
-        myRef.child(user.getUid()).child("Desks").equalTo(id_desk).addValueEventListener(new ValueEventListener() {
+        myRef.child(user.getUid()).child("Desks").child(id_desk).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot deskSnapshot: dataSnapshot.getChildren()) {
-                     desk = deskSnapshot.getValue(Desk.class);
-                }
-                name_desk = desk.getNameDesk();
+                Desk desk = dataSnapshot.getValue(Desk.class);
+                TextView title = (TextView) findViewById(R.id.toolbar_active_desk_page_title);
+                title.setText(desk.getNameDesk());
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
 
 
-        TextView title = (TextView) findViewById(R.id.toolbar_active_desk_page_title);
-        title.setText(this.name_desk);
+        pager = (ViewPager) findViewById(R.id.activeDesk_column);
+        pager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager()));
+        TabLayout tabLayout = findViewById(R.id.tab_layout_page);
+        tabLayout.setupWithViewPager(pager);
+
     }
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        private Fragment[] childFragments;
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+            childFragments = new Fragment[] {
+                    new PageFragment(context)
+            };
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return childFragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title = "Новый список";
+            return title.subSequence(title.lastIndexOf(".") + 1, title.length());
+        }
+
+    }
+
 
     public void closeActiveDeskPage(View v) {
         finish();
---
+++ app/src/main/java/com/androstock/multant/ActiveDesk/PageFragment.java
@@ -0,0 +1,85 @@
+package com.androstock.multant.ActiveDesk;
+
+import java.util.Random;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androstock.multant.R;
+
+public class PageFragment extends Fragment {
+
+    String name_column;
+    Context context;
+
+    public PageFragment(){
+    }
+
+    public PageFragment(Context context){
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.active_desk_add_name_column, container, false);
+        Button buttonInFragment = rootView.findViewById(R.id.button_add_column);
+        buttonInFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Получаем вид с файла prompt.xml, который применим для диалогового окна:
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.active_desk_prompt, null);
+
+                //Создаем AlertDialog
+                AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(context);
+
+                //Настраиваем prompt.xml для нашего AlertDialog:
+                mDialogBuilder.setView(promptsView);
+
+                //Настраиваем отображение поля для ввода текста в открытом диалоге:
+                final EditText userInput = (EditText) promptsView.findViewById(R.id.input_text);
+
+                //Настраиваем сообщение в диалоговом окне:
+                mDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        //Вводим текст и отображаем в строке ввода на основном экране:
+                                        name_column = userInput.getText().toString();
+                                    }
+                                })
+                        .setNegativeButton("Отмена",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                //Создаем AlertDialog:
+                AlertDialog alertDialog = mDialogBuilder.create();
+
+                //и отображаем его:
+                alertDialog.show();
+
+            }
+        });
+
+        return rootView;
+    }
+
+}
