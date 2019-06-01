b024e063e4c59bbb2ef2f75a313911794c14a60d
n3legal <nikita.shachnov@yandex.ru>
2019-06-01 10:43:58 +0300

    ласт

--- app/src/main/java/com/androstock/multant/ActiveDesk/ActiveDeskPage.java
+++ app/src/main/java/com/androstock/multant/ActiveDesk/ActiveDeskPage.java
@@ -16,9 +16,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.androstock.multant.R;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,31 +29,37 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ActiveDeskPage extends FragmentActivity {
 
     private FirebaseAuth mAuth;
     private DatabaseReference myRef;
+    FirebaseUser user = mAuth.getInstance().getCurrentUser();
 
     private String id_desk = "";
 
     ViewPager pager;
-    static final int PAGE_COUNT = 1;
 
-    final List<Column> columns = new ArrayList<Column>();
+    final List<String> columns = new ArrayList<String>();
+    final List<String> names_of_columns = new ArrayList<String>();
 
     final Context context = this;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.active_desk_page);
         this.id_desk = getIntent().getExtras().getString("id_desk");
-        FirebaseUser user = mAuth.getInstance().getCurrentUser();
+
 
         myRef = FirebaseDatabase.getInstance().getReference();
         myRef.child(user.getUid()).child("Desks").child(id_desk).addValueEventListener(new ValueEventListener() {
@@ -67,6 +76,33 @@ public class ActiveDeskPage extends FragmentActivity {
         });
 
 
+        myRef.child(user.getUid()).child("Desks").child(id_desk).child("Columns").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    String cl = postSnapshot.getKey();
+                    String nm = postSnapshot.child(cl).child("nameColumn").getValue(String.class);
+                    int n = 0;
+                    for (int i = 0;i<columns.size();i++){
+                        if (columns.get(i).equals(cl))
+                            n++;
+                    }
+                    if (n==0){
+                        columns.add(cl);
+                        names_of_columns.add(nm);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
         pager = (ViewPager) findViewById(R.id.activeDesk_column);
         pager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager()));
         TabLayout tabLayout = findViewById(R.id.tab_layout_page);
@@ -74,35 +110,70 @@ public class ActiveDeskPage extends FragmentActivity {
 
     }
 
+
+
     private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
 
-        private Fragment[] childFragments;
+        private Fragment[] childFragments = new Fragment[columns.size() + 1];
+
+
 
         public MyFragmentPagerAdapter(FragmentManager fm) {
             super(fm);
-            childFragments = new Fragment[] {
-                    new PageFragment(context, id_desk)
-            };
+            for (int i=0; i < columns.size() + 1; i++){
+                if (columns.size() == 0) {
+                    childFragments[i] = new PageFragment(context, id_desk, "", i, columns.size());
+                }else {
+                    childFragments[i] = new PageFragment(context, id_desk, columns.get(i), i, columns.size());
+                }
+
+            }
         }
 
         @Override
         public Fragment getItem(int position) {
-            return childFragments[position];
+            if (columns.size() == 0) {
+                return new PageFragment(context, id_desk, "", position, columns.size());
+            }else {
+                return new PageFragment(context, id_desk, columns.get(position), position, columns.size());
+            }
+
         }
 
         @Override
         public int getCount() {
-            return 1;
+            return columns.size() + 1;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            String title = "Новый список";
-            return title.subSequence(title.lastIndexOf(".") + 1, title.length());
+
+                String title;
+                if (position == columns.size()){
+                    title = "Новый список";
+                    return title.subSequence(title.lastIndexOf(".") + 1, title.length());
+                } else {
+                    /*myRef = FirebaseDatabase.getInstance().getReference();
+                    myRef.child(user.getUid()).child("Desks").child(id_desk)
+                            .child("Columns").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Column column = dataSnapshot.child(columns.get(position)).getValue(Column.class);
+                            title_page = column.getNameColumn();
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+                    title = title_page;*/
+                    title = names_of_columns.get(position);
+                    return title.subSequence(title.lastIndexOf(".") + 1, title.length());
+                }
         }
 
-    }
 
+
+    }
 
     public void closeActiveDeskPage(View v) {
         finish();
--- app/src/main/java/com/androstock/multant/ActiveDesk/PageFragment.java
+++ app/src/main/java/com/androstock/multant/ActiveDesk/PageFragment.java
@@ -2,6 +2,7 @@ package com.androstock.multant.ActiveDesk;
 
 import java.util.Random;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,6 +10,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +29,10 @@ public class PageFragment extends Fragment {
 
     String name_column;
     String id_desk;
+    String id_page;
     Context context;
+    int position;
+    int max_position;
 
     private FirebaseAuth mAuth;
     private DatabaseReference myRef;
@@ -37,9 +42,25 @@ public class PageFragment extends Fragment {
     public PageFragment(){
     }
 
-    public PageFragment(Context context, String id_desk){
+    @SuppressLint("ValidFragment")
+    public PageFragment(Context context, String id_desk, String id_page, int position, int max_position) {
+        Bundle arguments = new Bundle();
         this.context = context;
         this.id_desk = id_desk;
+        this.id_page = id_page;
+        this.position = position;
+        this.max_position = max_position;
+        arguments.putString("context", context.toString());
+        arguments.putString("id_desk", id_desk);
+        arguments.putString("id_page", id_page);
+        arguments.putString("position", String.valueOf(position));
+        arguments.putString("max_position", String.valueOf(max_position));
+        this.setArguments(arguments);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Nullable
@@ -47,58 +68,66 @@ public class PageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         myRef = FirebaseDatabase.getInstance().getReference();
-        View rootView = inflater.inflate(R.layout.active_desk_add_name_column, container, false);
-        Button buttonInFragment = rootView.findViewById(R.id.button_add_column);
-        buttonInFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Получаем вид с файла prompt.xml, который применим для диалогового окна:
-                LayoutInflater li = LayoutInflater.from(context);
-                View promptsView = li.inflate(R.layout.active_desk_prompt, null);
+        if (position == max_position){
+            View rootView = inflater.inflate(R.layout.active_desk_add_name_column, container, false);
+            Button buttonInFragment = rootView.findViewById(R.id.button_add_column);
+            buttonInFragment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Получаем вид с файла prompt.xml, который применим для диалогового окна:
+                    LayoutInflater li = LayoutInflater.from(context);
+                    View promptsView = li.inflate(R.layout.active_desk_prompt, null);
 
-                //Создаем AlertDialog
-                AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(context);
+                    //Создаем AlertDialog
+                    AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(context);
 
-                //Настраиваем prompt.xml для нашего AlertDialog:
-                mDialogBuilder.setView(promptsView);
+                    //Настраиваем prompt.xml для нашего AlertDialog:
+                    mDialogBuilder.setView(promptsView);
 
-                //Настраиваем отображение поля для ввода текста в открытом диалоге:
-                final EditText userInput = (EditText) promptsView.findViewById(R.id.input_text);
+                    //Настраиваем отображение поля для ввода текста в открытом диалоге:
+                    final EditText userInput = (EditText) promptsView.findViewById(R.id.input_text);
 
-                //Настраиваем сообщение в диалоговом окне:
-                mDialogBuilder
-                        .setCancelable(false)
-                        .setPositiveButton("OK",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        //Вводим текст и отображаем в строке ввода на основном экране:
-                                        name_column = userInput.getText().toString();
-                                        if (name_column.trim().length() < 1) {
-                                            Toast.makeText(context, "Введите название Колонны!", Toast.LENGTH_SHORT).show();
-                                        } else {
-                                            Toast.makeText(context, "Успешно!", Toast.LENGTH_SHORT).show();
-                                            String key = myRef.child(user.getUid()).child("Desks").child(id_desk).child("Columns").push().getKey();
-                                            myRef.child(user.getUid()).child("Desks").child(id_desk).child("Columns").child(key).setValue(new Column(name_column, key));
+                    //Настраиваем сообщение в диалоговом окне:
+                    mDialogBuilder
+                            .setCancelable(false)
+                            .setPositiveButton("OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog,int id) {
+                                            //Вводим текст и отображаем в строке ввода на основном экране:
+                                            name_column = userInput.getText().toString();
+                                            if (name_column.trim().length() < 1) {
+                                                Toast.makeText(context, "Введите название Колонны!", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Toast.makeText(context, "Успешно!", Toast.LENGTH_SHORT).show();
+                                                String key = myRef.child(user.getUid()).child("Desks").child(id_desk).child("Columns").push().getKey();
+                                                myRef.child(user.getUid()).child("Desks").child(id_desk).child("Columns").child(key).setValue(new Column(name_column, key));
+                                            }
                                         }
-                                    }
-                                })
-                        .setNegativeButton("Отмена",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
-                                        dialog.cancel();
-                                    }
-                                });
+                                    })
+                            .setNegativeButton("Отмена",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog,int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
 
-                //Создаем AlertDialog:
-                AlertDialog alertDialog = mDialogBuilder.create();
+                    //Создаем AlertDialog:
+                    AlertDialog alertDialog = mDialogBuilder.create();
 
-                //и отображаем его:
-                alertDialog.show();
+                    //и отображаем его:
+                    alertDialog.show();
 
-            }
-        });
+                }
+            });
+            return rootView;
+        } else {
+            View rootView = inflater.inflate(R.layout.active_desk_fragment_layout, container, false);
+            CardView cardView = rootView.findViewById(R.id.card_in_active_desk_column);
+            return rootView;
+        }
 
-        return rootView;
+
+
     }
 
 }
